ad8764367659e8df4f1ab145360b03a3a1f77fb7
Juraci Vieira <juraci.vieira@gmail.com>
2015-12-21 11:06:28 -0200

    Refactoring the request event to be more explicit

--- hello.js
+++ hello.js
@@ -1,8 +1,16 @@
 var http = require('http');
 
+/*
 http.createServer(function(request, response) {
     response.writeHead(200);
     response.end('Hello, this is Dog.');
 }).listen(8080);
+*/
+
+var server =  http.createServer().listen(8080);
+server.on('request', function(request, response) {
+    response.writeHead(200);
+    response.end('Hello, this is Dog.');
+});
 
 console.log('listening on 8080');
